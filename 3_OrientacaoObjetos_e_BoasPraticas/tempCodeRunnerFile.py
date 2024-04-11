7399f051e166fc94fa2f7a2ea70d63b7e13d7c88
bruno <b_linhares@outlook.com>
2024-04-11 17:12:16 -0300

    novo desafio + up readme

--- 3_OrientacaoObjetos_e_BoasPraticas/tempCodeRunnerFile.py
+++ 3_OrientacaoObjetos_e_BoasPraticas/tempCodeRunnerFile.py
@@ -1,19 +1,19 @@
-pessoas.append(PessoaFisica('444.555.666-11',
-    #                            'Carlos','22-07-1980',
-    #                            'rua de baixo, 12'))
-    # pessoas.append(PessoaFisica('123.432.123-11',
-    #                            'bruno','24-01-1990',
-    #                            'rua de cima, 123'))
-    # contas_correntes.append(ContaCorrente(1,pessoas[0]))
-    # contas_correntes.append(ContaCorrente(2,pessoas[0]))
-    # contas_correntes.append(ContaCorrente(3,pessoas[1]))
-    # transacao = Deposito(1000)
-    # transacao.registrar(contas_correntes[0])
-    # transacao = Deposito(140)
-    # transacao.registrar(contas_correntes[0])
-    # transacao = Deposito(500)
-    # transacao.registrar(contas_correntes[1])
-    # transacao = Deposito(10)
-    # transacao.registrar(contas_correntes[1])
-    # transacao = Deposito(300)
-    # transacao.registrar(contas_correntes[2])
+  pessoas.append(PessoaFisica('444.555.666-11',
+                               'Carlos','22-07-1980',
+                               'rua de baixo, 12'))
+    pessoas.append(PessoaFisica('123.432.123-11',
+                               'bruno','24-01-1990',
+                               'rua de cima, 123'))
+    contas_correntes.append(ContaCorrente(1,pessoas[0]))
+    contas_correntes.append(ContaCorrente(2,pessoas[0]))
+    contas_correntes.append(ContaCorrente(3,pessoas[1]))
+    transacao = Deposito(1000)
+    transacao.registrar(contas_correntes[0])
+    transacao = Deposito(140)
+    transacao.registrar(contas_correntes[0])
+    transacao = Deposito(500)
+    transacao.registrar(contas_correntes[1])
+    transacao = Deposito(10)
+    transacao.registrar(contas_correntes[1])
+    transacao = Deposito(300)
+    transacao.registrar(contas_correntes[2])
